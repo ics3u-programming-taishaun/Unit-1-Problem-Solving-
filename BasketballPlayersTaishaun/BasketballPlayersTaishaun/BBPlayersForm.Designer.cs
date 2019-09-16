42db613ae3a025a96a24371243f8dffaed620200
TaishaunJ <47784239+TaishaunJ@users.noreply.github.com>
2019-09-16 13:05:23 -0400

    push

--- BasketballPlayersTaishaun/BasketballPlayersTaishaun/BBPlayersForm.Designer.cs
+++ BasketballPlayersTaishaun/BasketballPlayersTaishaun/BBPlayersForm.Designer.cs
@@ -30,29 +30,66 @@
         {
             this.components = new System.ComponentModel.Container();
             this.grbPlayers = new System.Windows.Forms.GroupBox();
+            this.picGSW = new System.Windows.Forms.PictureBox();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.lblTeam = new System.Windows.Forms.Label();
             this.mnuBallPlayers = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.mnu = new System.Windows.Forms.MenuStrip();
             this.mnibasketballPlayers = new System.Windows.Forms.ToolStripMenuItem();
             this.mnilukaD = new System.Windows.Forms.ToolStripMenuItem();
             this.mniTraeY = new System.Windows.Forms.ToolStripMenuItem();
-            this.mniKlayT = new System.Windows.Forms.ToolStripMenuItem();
             this.mniJimmyB = new System.Windows.Forms.ToolStripMenuItem();
-            this.lblTeam = new System.Windows.Forms.Label();
-            this.lblPlayer = new System.Windows.Forms.Label();
+            this.mniKlayT = new System.Windows.Forms.ToolStripMenuItem();
             this.grbPlayers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGSW)).BeginInit();
             this.mnu.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbPlayers
             // 
+            this.grbPlayers.BackColor = System.Drawing.Color.White;
+            this.grbPlayers.Controls.Add(this.picGSW);
             this.grbPlayers.Controls.Add(this.lblPlayer);
             this.grbPlayers.Controls.Add(this.lblTeam);
-            this.grbPlayers.Location = new System.Drawing.Point(65, 43);
+            this.grbPlayers.Location = new System.Drawing.Point(337, 215);
             this.grbPlayers.Name = "grbPlayers";
-            this.grbPlayers.Size = new System.Drawing.Size(588, 372);
+            this.grbPlayers.Size = new System.Drawing.Size(715, 439);
             this.grbPlayers.TabIndex = 0;
             this.grbPlayers.TabStop = false;
-            this.grbPlayers.Text = "groupBox1";
+            this.grbPlayers.Text = "NBA";
+            // 
+            // picGSW
+            // 
+            this.picGSW.Location = new System.Drawing.Point(444, 146);
+            this.picGSW.Name = "picGSW";
+            this.picGSW.Size = new System.Drawing.Size(231, 222);
+            this.picGSW.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGSW.TabIndex = 2;
+            this.picGSW.TabStop = false;
+            this.picGSW.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.ForeColor = System.Drawing.Color.Blue;
+            this.lblPlayer.Location = new System.Drawing.Point(203, 173);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(97, 35);
+            this.lblPlayer.TabIndex = 1;
+            this.lblPlayer.Text = "label2";
+            this.lblPlayer.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // lblTeam
+            // 
+            this.lblTeam.AutoSize = true;
+            this.lblTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeam.ForeColor = System.Drawing.Color.Red;
+            this.lblTeam.Location = new System.Drawing.Point(170, 45);
+            this.lblTeam.Name = "lblTeam";
+            this.lblTeam.Size = new System.Drawing.Size(173, 63);
+            this.lblTeam.TabIndex = 0;
+            this.lblTeam.Text = "label1";
             // 
             // mnuBallPlayers
             // 
@@ -65,16 +102,16 @@
             this.mnibasketballPlayers});
             this.mnu.Location = new System.Drawing.Point(0, 0);
             this.mnu.Name = "mnu";
-            this.mnu.Size = new System.Drawing.Size(800, 24);
+            this.mnu.Size = new System.Drawing.Size(1331, 24);
             this.mnu.TabIndex = 2;
             // 
             // mnibasketballPlayers
             // 
             this.mnibasketballPlayers.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnilukaD,
+            this.mniJimmyB,
             this.mniTraeY,
-            this.mniKlayT,
-            this.mniJimmyB});
+            this.mniKlayT});
             this.mnibasketballPlayers.Name = "mnibasketballPlayers";
             this.mnibasketballPlayers.Size = new System.Drawing.Size(165, 20);
             this.mnibasketballPlayers.Text = "Basketball Players and team";
@@ -91,46 +128,29 @@
             this.mniTraeY.Name = "mniTraeY";
             this.mniTraeY.Size = new System.Drawing.Size(180, 22);
             this.mniTraeY.Text = "Trae Young";
-            // 
-            // mniKlayT
-            // 
-            this.mniKlayT.Name = "mniKlayT";
-            this.mniKlayT.Size = new System.Drawing.Size(180, 22);
-            this.mniKlayT.Text = "Klay Thompson";
+            this.mniTraeY.Click += new System.EventHandler(this.MniTraeY_Click);
             // 
             // mniJimmyB
             // 
             this.mniJimmyB.Name = "mniJimmyB";
             this.mniJimmyB.Size = new System.Drawing.Size(180, 22);
             this.mniJimmyB.Text = "Jimmy Butler";
+            this.mniJimmyB.Click += new System.EventHandler(this.MniJimmyB_Click);
             // 
-            // lblTeam
+            // mniKlayT
             // 
-            this.lblTeam.AutoSize = true;
-            this.lblTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTeam.Location = new System.Drawing.Point(116, 46);
-            this.lblTeam.Name = "lblTeam";
-            this.lblTeam.Size = new System.Drawing.Size(173, 63);
-            this.lblTeam.TabIndex = 0;
-            this.lblTeam.Text = "label1";
-            // 
-            // lblPlayer
-            // 
-            this.lblPlayer.AutoSize = true;
-            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayer.Location = new System.Drawing.Point(192, 215);
-            this.lblPlayer.Name = "lblPlayer";
-            this.lblPlayer.Size = new System.Drawing.Size(97, 35);
-            this.lblPlayer.TabIndex = 1;
-            this.lblPlayer.Text = "label2";
-            this.lblPlayer.Click += new System.EventHandler(this.Label2_Click);
+            this.mniKlayT.Name = "mniKlayT";
+            this.mniKlayT.Size = new System.Drawing.Size(180, 22);
+            this.mniKlayT.Text = "Klay Thompson";
+            this.mniKlayT.Click += new System.EventHandler(this.MniKlayT_Click_1);
             // 
             // frmballPlayers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::BasketballPlayersTaishaun.Properties.Resources.nba;
+            this.ClientSize = new System.Drawing.Size(1331, 791);
             this.Controls.Add(this.mnu);
             this.Controls.Add(this.grbPlayers);
             this.MainMenuStrip = this.mnu;
@@ -139,6 +159,7 @@
             this.Load += new System.EventHandler(this.BBPlayersForm_Load);
             this.grbPlayers.ResumeLayout(false);
             this.grbPlayers.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGSW)).EndInit();
             this.mnu.ResumeLayout(false);
             this.mnu.PerformLayout();
             this.ResumeLayout(false);
@@ -156,8 +177,9 @@
         private System.Windows.Forms.ToolStripMenuItem mnibasketballPlayers;
         private System.Windows.Forms.ToolStripMenuItem mnilukaD;
         private System.Windows.Forms.ToolStripMenuItem mniTraeY;
-        private System.Windows.Forms.ToolStripMenuItem mniKlayT;
         private System.Windows.Forms.ToolStripMenuItem mniJimmyB;
+        private System.Windows.Forms.ToolStripMenuItem mniKlayT;
+        private System.Windows.Forms.PictureBox picGSW;
     }
 }
 
--- BasketballPlayersTaishaun/BasketballPlayersTaishaun/BBPlayersForm.cs
+++ BasketballPlayersTaishaun/BasketballPlayersTaishaun/BBPlayersForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Taishaun Johnson
+ * Created on: sept.13, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - Basketball Players 
+ * This program displays team name, star player and team logo.
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,10 +34,43 @@ namespace BasketballPlayersTaishaun
 
         }
 
+        // Add team name Dallas and star player Doncic and show team logo.
         private void MnilukaD_Click(object sender, EventArgs e)
         {
             this.lblTeam.Text = "Dallas Mavericks";
             this.lblPlayer.Text = "Luka Doncic";
+            picGSW.Image = (Properties.Resources.mavericks_logo_dallas_sports_fanatic);
+        }
+
+        // Add team name Atlanta and star player Young and show team logo.
+        private void MniTraeY_Click(object sender, EventArgs e)
+        {
+            this.lblTeam.Text = "Atlanta Hawks";
+            this.lblPlayer.Text = "Trae Young";
+            picGSW.Image = (Properties.Resources.Atlanta);
+               
+        }
+
+        // Add team name Miami and star player Butler and show team logo.
+        private void MniJimmyB_Click(object sender, EventArgs e)
+        {
+            this.lblTeam.Text = "Miami Heat";
+            this.lblPlayer.Text = "Jimmy Butler";
+            picGSW.Image = (Properties.Resources.black_miami_heat_logo);
+
+        }
+
+        // Add team name GSW and star player Thompson and show team logo.
+        private void MniKlayT_Click_1(object sender, EventArgs e)
+        {
+            this.lblTeam.Text = "GS Warriors";
+            this.lblPlayer.Text = "Klay Thompson";
+            picGSW.Image = (Properties.Resources.warriors);
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
